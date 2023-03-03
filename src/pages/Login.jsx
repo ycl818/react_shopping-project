2e1d07a149610f074c2c37cb3e36e1c709e06fb8
yichien <kevinyichienlee@gmail.com>
2023-03-03 23:21:37 +0800

    add login error msg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,20 +11,30 @@ function Login() {
     password: ''
   });
 
+  const [loginState, setLoginState] = useState({})
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setData({...data, [name]: value});
   }
 
   const submit = async (e) => {
-    const res = await axios.post('/v2/admin/signin', data);
-    const { token, expired } = res.data;
-    console.log(res)
-    document.cookie = `hexToken=${token}; expires=${new Date(expired)}; `;
 
-    if (res.data.success) {
-      navigate('/admin/products')
+    try {
+      const res = await axios.post('/v2/admin/signin', data);
+      const { token, expired } = res.data;
+      console.log(res)
+      document.cookie = `hexToken=${token}; expires=${new Date(expired)}; `;
+  
+      if (res.data.success) {
+        navigate('/admin/products')
+      }
+
+    } catch (error) {
+      console.log(error)
+      setLoginState(error.response.data)
     }
+
   }
 
   
@@ -35,8 +45,8 @@ function Login() {
       <div className="col-md-6">
         <h2>登入帳號</h2>
 
-        <div className="alert alert-danger" role="alert">
-          錯誤訊息
+        <div className={`alert alert-danger ${loginState.message ? 'd-block' : 'd-none'}`} role="alert">
+          {loginState.message}
         </div>
         <div className="mb-2">
           <label htmlFor="email" className="form-label w-100">
