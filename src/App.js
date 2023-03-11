815ac0e28830f8e8a9696cd16313ab312b2c67c0
yichien <kevinyichienlee@gmail.com>
2023-03-11 23:30:49 +0800

    add frontlayout and homw

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Dashboard from "./pages/admin/Dashboard";
 import AdminProducts from "./pages/admin/AdminProducts";
 import AdminCoupons from "./pages/admin/AdminCoupons";
 import AdminOrders from "./pages/admin/AdminOrder";
+import FrontLayout from "./pages/front/FrontLayout";
+import Home from "./pages/front/Home";
+import Products from "./pages/front/Products";
 
 function App() {
 
@@ -11,6 +14,10 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={  <FrontLayout/> }>
+          <Route path="" element={ <Home/> }></Route>
+          <Route path="products" element={ <Products/> }></Route>
+        </Route>
         <Route path="/login" element={ <Login /> }></Route>
         <Route path="/admin" element={ <Dashboard /> }>
           <Route path="products" element={ <AdminProducts/> } ></Route>
